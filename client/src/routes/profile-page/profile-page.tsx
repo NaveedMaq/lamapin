8a0bbea9deb8c04741f53ef9407378334c64a99e
Naveed <45787041+NaveedMaq@users.noreply.github.com>
2025-03-16 18:05:43 +0530

    feat: enhance profile page layout with styling and interactive elements

--- client/src/routes/profile-page/profile-page.tsx
+++ client/src/routes/profile-page/profile-page.tsx
@@ -1,5 +1,28 @@
+import { Image } from '../../components/image/image';
 import './profile-page.css';
 
 export const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className='profilePage'>
+      <Image path='/general/noAvatar.png' />
+      <h1 className='profileName'>John Doe</h1>
+      <span className='profileUsername'>@johndoe</span>
+      <div className='followCounts'>10 followers &middot; 20 followings</div>
+
+      <div className='profileInteractions'>
+        <Image path='/general/share.svg' />
+
+        <div className='profileButtons'>
+          <button>Message</button>
+          <button>Follow</button>
+        </div>
+        <Image path='/general/more.svg' />
+      </div>
+
+      <div className='profileOptions'>
+        <span>Created</span>
+        <span>Saved</span>
+      </div>
+    </div>
+  );
 };
