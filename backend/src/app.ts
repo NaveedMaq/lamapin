67c1a6e34330785124a6fef44c46a26cb5e7f1af
Naveed <45787041+NaveedMaq@users.noreply.github.com>
2025-04-20 17:18:34 +0530

    feat: Add basic route structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import boardRouter from './routes/board.route';
+import commentRouter from './routes/comment.route';
+import pinRouter from './routes/pin.route';
+import userRouter from './routes/user.route';
+import { connectDb } from './utils/connectDb';
 
 const app = express();
 
+app.use('/boards', boardRouter);
+app.use('/comments', commentRouter);
+app.use('/pins', pinRouter);
+app.use('/users', userRouter);
+
 app.listen(3500, () => {
+  connectDb();
   console.log('Server is running');
 });
